0b7e37aedc19988d130784e5ede4409ed4de9b85
Ṕriscila Araújo <araujo16012002@gmail.com>
2019-07-22 15:33:14 -0300

    Adicionar for no script

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let nomeTartaruga = "Ângelo";
+/*let nomeTartaruga = "Ângelo";
 nomeTartaruga = contador = 0;
 
 while (contador<10){
     console.log(nomeTartaruga);
     contador ++;
+}*/
+
+
+
+let listaDeCornos = ["Fernando Furtivo", "Hugo Felipe de Votuy", "Tefunor da Silva Bednou", "Kugo da Silva", "Jorjanonei Dlogo"];
+
+for(let contador = 0; contador<listaDeCornos.length; contador++){
+    console.log (listaDeCornos[contador]);
 }
