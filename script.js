8245f21aea0a2ebf8295cbb7f85c64f1fc0f4b7c
Ṕriscila Araújo <araujo16012002@gmail.com>
2019-07-22 16:25:23 -0300

    Adicionar let filmes

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ for(let contador = 0; contador<listaDeCornos.length; contador++){
 
 
 
-let listaDeCornos = ["Fernando Furtivo", "Hugo Felipe de Votuy", "Tefunor da Silva Bednou", "Kugo da Silva", "Jorjanonei Dlogo"];
+/*let listaDeCornos = ["Fernando Furtivo", "Hugo Felipe de Votuy", "Tefunor da Silva Bednou", "Kugo da Silva", "Jorjanonei Dlogo"];
 
 for(let numero in listaDeCornos){
     console.log (numero);
@@ -24,4 +24,28 @@ for(let numero in listaDeCornos){
 
 for(let aluno of listaDeCornos){
     console.log (aluno);
+}*/
+
+
+
+/*let listaDeCornos = ["Fernando Furtivo", "Hugo Felipe de Votuy", "Tefunor da Silva Bednou", "Kugo da Silva", "Jorjanonei Dlogo"];
+
+for(let numero in listaDeCornos){
+    console.log (numero);
+}
+
+for(let aluno of listaDeCornos){
+    console.log (aluno);
+}*/
+
+
+
+let filmes = [
+    {nome: "Rei Leão", classificação: 12, estilo: Animação},
+    {nome: "Corajosos", classificação: 16, estilo: Ação},
+    {nome: "Como se fosse a primeira vez", classificação: 18, estilo: Romace}
+];
+
+for(let filmes of filmes){
+    console.log(`O filme ${filme.nome} tem a classifição de ${classificação} anos, e o estilo do filme é ${estilo}.`)
 }
