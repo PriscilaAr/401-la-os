d68116d47c2c4f2da4ee196f200e8974c883f2ac
Ṕriscila Araújo <araujo16012002@gmail.com>
2019-07-22 15:38:53 -0300

    Adicionar for in e for of no script

--- script.js
+++ script.js
@@ -8,8 +8,20 @@ while (contador<10){
 
 
 
-let listaDeCornos = ["Fernando Furtivo", "Hugo Felipe de Votuy", "Tefunor da Silva Bednou", "Kugo da Silva", "Jorjanonei Dlogo"];
+/*let listaDeCornos = ["Fernando Furtivo", "Hugo Felipe de Votuy", "Tefunor da Silva Bednou", "Kugo da Silva", "Jorjanonei Dlogo"];
 
 for(let contador = 0; contador<listaDeCornos.length; contador++){
     console.log (listaDeCornos[contador]);
+}*/
+
+
+
+let listaDeCornos = ["Fernando Furtivo", "Hugo Felipe de Votuy", "Tefunor da Silva Bednou", "Kugo da Silva", "Jorjanonei Dlogo"];
+
+for(let numero in listaDeCornos){
+    console.log (numero);
+}
+
+for(let aluno of listaDeCornos){
+    console.log (aluno);
 }
